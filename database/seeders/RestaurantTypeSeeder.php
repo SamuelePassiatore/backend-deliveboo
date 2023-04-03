092a8a1f9a47af6c6d041066b47f27b9aad8bb44
SamuelePassiatore <semmisno2@protonmail.com>
2023-04-03 17:45:21 +0200

    created restaurant type seeder

---
+++ database/seeders/RestaurantTypeSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RestaurantTypeSeeder extends Seeder
+{
+    public function run()
+    {
+        $restaurants_types = [
+            ['restaurant_id' => 1, 'type_id' => 1],
+            ['restaurant_id' => 2, 'type_id' => 2],
+            ['restaurant_id' => 3, 'type_id' => 3],
+            ['restaurant_id' => 4, 'type_id' => 4],
+            ['restaurant_id' => 5, 'type_id' => 5],
+            ['restaurant_id' => 6, 'type_id' => 6],
+            ['restaurant_id' => 7, 'type_id' => 7],
+        ];
+
+        foreach ($restaurants_types as $restaurant_type) {
+            DB::table('restaurant_type')->insert([
+                'restaurant_id' => $restaurant_type['restaurant_id'],
+                'type_id' => $restaurant_type['type_id'],
+            ]);
+        }
+    }
+}
