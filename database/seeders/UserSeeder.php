8a943697070fad9c0a6dd4ecf1a07831c27e4889
SamuelePassiatore <semmisno2@protonmail.com>
2023-04-03 10:42:28 +0200

    added user seeder with foreach

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users =  [
+            ["name" => "Davide Cardelli", "email" => "davide@example.com", "password" => "password"],
+            ["name" => "Samuele Passiatore", "email" => "samuele@example.com", "password" => "password"],
+            ["name" => "Gianmaria Fatta", "email" => "gianmaria@example.com", "password" => "password"],
+            ["name" => "Michela Ricci", "email" => "michela@example.com", "password" => "password"],
+            ["name" => "Giuseppe Dattilo", "email" => "giuseppe@example.com", "password" => "password"],
+            ["name" => "Paolo Marroni", "email" => "paolo.marroni@example.com", "password" => "password"],
+            ["name" => "Chiara Arancioni", "email" => "chiara.arancioni@example.com", "password" => "password"],
+            ["name" => "Alessandro Azzurri", "email" => "alessandro.azzurri@example.com", "password" => "password"]
+        ];
+
+        foreach ($users as $user) {
+            $new_user = new User();
+            $new_user->name = $user['name'];
+            $new_user->email = $user['email'];
+            $new_user->password = $user['password'];
+            $new_user->save();
+        }
+    }
+}
