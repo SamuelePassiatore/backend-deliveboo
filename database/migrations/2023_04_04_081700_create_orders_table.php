5b818b0b8af0be06bd37e9ceb562a89cfd4d0645
SamuelePassiatore <semmisno2@protonmail.com>
2023-04-04 10:22:35 +0200

    created migration orders

---
+++ database/migrations/2023_04_04_081700_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name', 60);
+            $table->string('last_name', 60);
+            $table->string('mail', 60);
+            $table->string('phone', 15);
+            $table->string('address', 255);
+            $table->decimal('total_amount', 6, 2)->unsigned();
+            $table->char('order_code', 8)->unique();
+            $table->boolean('status');
+            $table->text('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
