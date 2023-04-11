e71ce0cc76297c1d37bd8722c74da052a32a0348
SamuelePassiatore <semmisno2@protonmail.com>
2023-04-11 23:40:58 +0200

    changed style of update profile information

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,10 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-        <h2 class="fs-4 text-secondary my-4">
-            {{ __('Profilo') }}
-        </h2>
-        <div class="card p-4 mb-4 bg-white shadow rounded-lg">
+        <div class="card p-4 mb-4 bg-white shadow rounded-lg mt-4">
 
             @include('profile.partials.update-profile-information-form')
 
