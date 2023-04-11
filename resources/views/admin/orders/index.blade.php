9e5ef9cec63d8e3419930e3cda9b38e56e0bc1da
SamuelePassiatore <semmisno2@protonmail.com>
2023-04-11 19:30:06 +0200

    change style of index and show orders

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -3,67 +3,68 @@
 @section('title', 'Ordini')
 
 @section('content')
-    <div class="container">
-        <header class="my-4 d-flex align-items-center justify-content-between">
-            @foreach ($orders as $order)
-                <h1>I tuoi ordini - {{ $order->restaurant->restaurant_name }}</h1>
-            @endforeach
-            <div>
-                <a href="{{ route('admin.restaurants.index') }}" class="btn btn-secondary">
-                    <i class="fas fa-arrow-left me-2"></i>Indietro
-                </a>
-            </div>
-        </header>
+    <div class="index-main d-flex flex-column justify-content-between">
+        <div class="container">
+            <header class="pt-4 pb-3 d-flex align-items-center justify-content-between">
+                @foreach ($orders as $order)
+                    <h1 class="text-dark-green">I tuoi ordini - {{ $order->restaurant->restaurant_name }}</h1>
+                @endforeach
+            </header>
 
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Codice Ordine</th>
-                    <th scope="col">Nome</th>
-                    <th scope="col">Cognome</th>
-                    <th scope="col">Piatti</th>
-                    <th scope="col">Totale</th>
-                    <th scope="col">Status</th>
-                    <th scope="col"></th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse ($orders as $order)
+            <table class="table">
+                <thead>
                     <tr>
-                        <th scope="row">{{ $order->id }}</th>
-                        <td>{{ $order->order_code }}</td>
-                        <td>{{ $order->first_name }}</td>
-                        <td>{{ $order->last_name }}</td>
-                        <td>
-                            @forelse ($order->plates as $plate)
-                                <span>
-                                    {{ $plate->name }}
-                                    @if (!$loop->last)
-                                        ,
-                                    @endif
-                                </span>
-                            @empty
-                                <div class="text-center">-</div>
-                            @endforelse
-                        </td>
-                        <td>{{ $order->total_amount }} €</td>
-                        <td>{{ $order->status ? 'Riuscito' : 'Respinto' }}</td>
-                        <td>
-                            <div class="d-flex">
-                                <a class="btn btn-sm btn-primary" href="{{ route('admin.orders.show', $plate->id) }}">
-                                    <i class="fas fa-eye"></i>
-                                </a>
-                            </div>
-                        </td>
+                        <th scope="col" class="text-dark-green">Codice Ordine</th>
+                        <th scope="col" class="text-dark-green">Nome</th>
+                        <th scope="col" class="text-dark-green">Cognome</th>
+                        <th scope="col" class="text-dark-green">Piatti</th>
+                        <th scope="col" class="text-dark-green">Totale</th>
+                        <th scope="col" class="text-dark-green">Status</th>
+                        <th scope="col"></th>
                     </tr>
-                @empty
-                    <tr>
-                        <td scope="row" colspan="7" class="text-center">Non ci sono ordini al momento</td>
-                    </tr>
-                @endforelse
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @forelse ($orders as $order)
+                        <tr>
+                            <td>{{ $order->order_code }}</td>
+                            <td>{{ $order->first_name }}</td>
+                            <td>{{ $order->last_name }}</td>
+                            <td>
+                                @forelse ($order->plates as $plate)
+                                    <span>
+                                        {{ $plate->name }}
+                                        @if (!$loop->last)
+                                            ,
+                                        @endif
+                                    </span>
+                                @empty
+                                    <div class="text-center">-</div>
+                                @endforelse
+                            </td>
+                            <td>{{ $order->total_amount }} €</td>
+                            <td>{{ $order->status ? 'Riuscito' : 'Respinto' }}</td>
+                            <td>
+                                <div class="d-flex">
+                                    <a class="btn btn-sm btn-white border-dark"
+                                        href="{{ route('admin.orders.show', $plate->id) }}">
+                                        <i class="fas fa-eye"></i>
+                                    </a>
+                                </div>
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td scope="row" colspan="7" class="text-center">Non ci sono ordini al momento</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+        <div class="d-flex justify-content-end mb-2 me-4">
+            <a href="{{ route('admin.restaurants.index') }}" class="btn btn-white border-dark">
+                <i class="fas fa-arrow-left"></i>
+            </a>
+        </div>
     </div>
 
 @endsection
--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -3,57 +3,66 @@
 @section('title', $order->order_code)
 
 @section('content')
+    <div class="index-main d-flex flex-column justify-content-between">
+        <div class="container">
+            <header class="pt-5 text-center">
+                <h1 class="text-dark-green">{{ $order->first_name }} {{ $order->last_name }} - Numero ordine:
+                    {{ $order->order_code }}</h1>
+            </header>
 
-    <div class="container">
-        <header class="pt-5 text-center">
-            <h1>{{ $order->first_name }} {{ $order->last_name }} - Numero ordine: {{ $order->order_code }}</h1>
-        </header>
-
-        <section id="single-restaurant">
-            <div class="container">
-                <div class="row row-cols-2">
-                    {{-- ORDER CONTENT --}}
-                    <div class="col d-flex justify-content-center flex-column pt-5">
-                        <div class="my-2"><strong>Nome: </strong> {{ $order->first_name }}</div>
-                        <div class="my-2"><strong>Cognome: </strong> {{ $order->last_name }}</div>
-                        <div class="my-2"><strong>Email: </strong> {{ $order->mail }}</div>
-                        <div class="my-2"><strong>Telefono: </strong> {{ $order->phone }}</div>
-                        <div class="my-2"><strong>Indirizzo: </strong> {{ $order->address }}</div>
-                        <div class="my-2"><strong>Piatti: </strong>
-                            @forelse ($order->plates as $plate)
-                                <span>
-                                    {{ $plate->name }}
-                                    @if (!$loop->last)
-                                        ,
-                                    @endif
-                                </span>
-                            @empty
-                                <div class="text-center">-</div>
-                            @endforelse
-                        </div>
-                        <div class="my-2"><strong>Totale: </strong> {{ $order->total_amount }} €</div>
-                        <div class="my-2"><strong>Codice Ordine: </strong> {{ $order->order_code }}</div>
-                        <div class="my-2"><strong>Status: </strong> {{ $order->status ? 'Riuscito' : 'Respinto' }}</div>
-                        @if ($order->note)
-                            <div><strong>Note: </strong>
-                                <p class="my-2"> {{ $order->note }}</p>
+            <section id="single-restaurant">
+                <div class="container">
+                    <div class="row row-cols-2">
+                        {{-- ORDER CONTENT --}}
+                        <div class="col d-flex justify-content-center flex-column pt-5">
+                            <div class="my-2"><strong class="text-dark-green">Nome: </strong> {{ $order->first_name }}</div>
+                            <div class="my-2"><strong class="text-dark-green">Cognome: </strong> {{ $order->last_name }}
                             </div>
-                        @else
-                            <span>-</span>
-                        @endif
+                            <div class="my-2"><strong class="text-dark-green">Email: </strong> {{ $order->mail }}</div>
+                            <div class="my-2"><strong class="text-dark-green">Telefono: </strong> {{ $order->phone }}
+                            </div>
+                            <div class="my-2"><strong class="text-dark-green">Indirizzo: </strong> {{ $order->address }}
+                            </div>
+                            <div class="my-2"><strong class="text-dark-green">Piatti: </strong>
+                                @forelse ($order->plates as $plate)
+                                    <span>
+                                        {{ $plate->name }}
+                                        @if (!$loop->last)
+                                            ,
+                                        @endif
+                                    </span>
+                                @empty
+                                    <div class="text-center">-</div>
+                                @endforelse
+                            </div>
+                            <div class="my-2"><strong class="text-dark-green">Totale: </strong> {{ $order->total_amount }}
+                                €
+                            </div>
+                            <div class="my-2"><strong class="text-dark-green">Codice Ordine: </strong>
+                                {{ $order->order_code }}</div>
+                            <div class="my-2"><strong class="text-dark-green">Status: </strong>
+                                {{ $order->status ? 'Riuscito' : 'Respinto' }}</div>
+                            @if ($order->note)
+                                <div class="my-2">
+                                    <strong class="text-dark-green">Note: </strong>
+                                    <p> {{ $order->note }}</p>
+                                </div>
+                            @else
+                                <span>-</span>
+                            @endif
 
+                        </div>
                     </div>
                 </div>
-
-                {{-- BUTTONS --}}
-                <div class="d-flex justify-content-between my-5">
-                    <a href="{{ route('admin.orders.index') }}" class="btn btn-secondary">
-                        <i class="fas fa-arrow-left me-2"></i>Indietro
-                    </a>
-                </div>
-            </div>
-        </section>
+            </section>
+        </div>
+        <div class="d-flex justify-content-end mb-2 me-4">
+            <a href="{{ route('admin.orders.index') }}" class="btn btn-white border-dark">
+                <i class="fas fa-arrow-left"></i>
+            </a>
+        </div>
     </div>
+
 @endsection
 
 @section('scripts')
