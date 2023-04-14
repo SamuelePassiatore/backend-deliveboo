6cb69678d74a2aeec43aa061234c325757a5a808
SamuelePassiatore <semmisno2@protonmail.com>
2023-04-14 17:26:08 +0200

    removed function purchase from cart controller

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -12,37 +12,6 @@ use Faker\Generator as Faker;
 
 class CartController extends Controller
 {
-    public function purchase(Request $request, Faker $faker)
-    {
-
-        $new_order = new Order();
-
-        $new_order->order_code = $faker->ean8();
-        $new_order->first_name = $request->first_name;
-        $new_order->last_name = $request->last_name;
-        $new_order->address = $request->address;
-        $new_order->mail = $request->mail;
-        $new_order->phone = $request->phone;
-        $new_order->date = today();
-        $new_order->total_amount = $request->total_amount;
-        $new_order->note = $request->note;
-        $new_order->status = $request->status;
-        $new_order->save();
-
-        $list_item = [];
-        foreach ($request->order_plate as $item) {
-            $key = $item['id'];
-            $quantity = $item['quantity'];
-            $price = $item['price'];
-        }
-        $new_order->plates()->attach($list_item);
-
-        return response()->json([
-            'results' => $request->all(),
-            'order' => $new_order
-        ]);
-    }
-
     public function generate(Request $request, Gateway $gateway)
     {
         $token = $gateway->clientToken()->generate();
@@ -55,9 +24,10 @@ class CartController extends Controller
 
     public function makePayment(OrderPaymentRequest $request, Gateway $gateway)
     {
+        $plate = Plate::find($request->plate);
 
         $result = $gateway->transaction()->sale([
-            'amount' =>  $request->price,
+            'amount' =>  $plate->price,
             'paymentMethodNonce' => $request->token,
             'options' => [
                 'submitForSettlement' => true
--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::apiResource('types', TypeController::class);
 Route::apiResource('restaurants', RestaurantController::class);
 
-Route::post('purchase', [CartController::class, 'purchase']);
-
 Route::get('orders/generate', [CartController::class, 'generate']);
 Route::post('orders/make/payment', [CartController::class, 'makePayment']);
