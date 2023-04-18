68a6325746a41961cb1ab016cfc1f8d03cc28a48
SamuelePassiatore <semmisno2@protonmail.com>
2023-04-18 12:57:59 +0200

    added condition in handler to extend 404 view to all errors pages & added icon to site

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,10 +3,21 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof ModelNotFoundException) {
+            return response()->view('404', [], 404);
+        }
+
+        return parent::render($request, $exception);
+    }
+
     /**
      * A list of exception types with their corresponding custom log levels.
      *
--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="index-main">
-        <div class="d-flex flex-column align-items-center justify-content-center">
+        <div class="d-flex flex-column align-items-center justify-content-center pt-3">
             <h1>404 | Not Found</h1>
             <img class="img-fluid vert-move mb-3" src="{{ Vite::asset('resources/img/triste-404.png') }}" alt="">
             <div class="d-flex justify-content-end mb-2 me-4">
