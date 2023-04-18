828f8ea52019b4168b55d8fd4acb0596e7ce75a5
SamuelePassiatore <semmisno2@protonmail.com>
2023-04-18 13:12:18 +0200

    changed alerts style

--- resources/views/includes/alerts/session.blade.php
+++ resources/views/includes/alerts/session.blade.php
@@ -1,7 +1,8 @@
 <div class="container">
     @if (session('message'))
-        <div class="alert alert-{{ session('type') ?? 'info' }} mt-4">
+        <div class="alert alert-{{ session('type') ?? 'info' }} mt-4 alert-dismissible fade show" role="alert">
             {{ session('message') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     @endif
 </div>
