db8d30c8ca00608943272bc9e8c9f1665938fb44
SamuelePassiatore <semmisno2@protonmail.com>
2023-04-18 10:47:39 +0200

    added alerts session in app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
     <div id="app">
         @include('includes.navbar')
         <main>
-            {{-- @include('includes.alerts.session') --}}
+            @include('includes.alerts.session')
             @yield('content')
         </main>
         <footer>
